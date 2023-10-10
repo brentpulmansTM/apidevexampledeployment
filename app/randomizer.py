b8ba86ebce5e295c107632fff7ff4b3be5cceb4d
Brent Pulmans <brent.pulmans@thomasmore.be>
2023-10-10 11:04:01 +0200

    Endpoint added

--- app/randomizer.py
+++ app/randomizer.py
@@ -3,8 +3,10 @@ from random import randint
 
 app = FastAPI()
 
-#123
-
 @app.get("/percentage")
 async def get_random_percentage():
     return {'percentage': randint(0, 100)}
+
+@app.get("/limits")
+async def get_random_percentage_with_limits(lower_limit: int, upper_limit: int):
+    return {'percentage': randint(lower_limit, upper_limit)}
